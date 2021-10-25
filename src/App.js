ab3ccfdba9d1f505a0ed632970e2fcaf2ad4129e
jhunethan <jhunethan2812@gmail.com>
2021-10-25 12:10:20 +0100

    added input validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [factors, setFactors] = useState();
+  const [number, setNumber] = useState();
+  const [displayNumber, setDisplayNumber] = useState();
+
+  function findFactors(event) {
+    if (event) event.preventDefault();
+
+    setDisplayNumber(isNaN(number) ? `${number} is not a number.` : number);
+
+    const tempFactors = [];
+
+    for (let i = number; i > 0; i--) {
+      if (number % i === 0) {
+        tempFactors.push(+i);
+      }
+    }
+
+    setFactors([...tempFactors]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Alacrity Coding Challenge</h1>
+      <section className="info__container section">
+        <p>Enter a number to see if it is a prime number.<br/> 
+          If it isn't, it's factors will be revealed.<br/>
+          Positive numbers only</p>
+      </section>
+      <main className="main__container">
+        <form onSubmit={findFactors} className="section">
+          <h2>Enter a number</h2>
+          <input
+            type="text"
+            className="main__input input-group mb-3 w-50"
+            onChange={(event) => setNumber(event.target.value)}
+          />
+          <button className="btn btn-primary">Start</button>
+        </form>
+
+        {displayNumber && (
+          <h2>
+            {displayNumber}
+            {factors && factors.length === 2
+              ? `is a prime number!`
+              : factors && !isNaN(displayNumber) ?
+                `is not a prime number.` : <div>Please enter a valid number</div>}
+          </h2>
+        )}
+
+        {factors && factors.length > 2 && (
+          <div className="factors__list">
+            <h2>Factors of {displayNumber}</h2>
+            {factors.map((num) => (
+              <div>{num}</div>
+            ))}
+          </div>
+        )}
+      </main>
     </div>
   );
 }
